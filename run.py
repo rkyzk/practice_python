790d44550750d3d659dd77788e113f39df76183c
rkyzk <reikoyazaki@yahoo.com>
2022-11-21 08:59:55 +0000

    make hangman game

--- run.py
+++ run.py
@@ -1,53 +1,28 @@
 import random
 
-score_comp = 0
-score_user = 0
-options = ["Rock", "Scissors", "Paper"]
+words = []
+with open('words.py') as f: 
+    words = f.read().splitlines()
 
-while True:
-    num = random.randrange(3)
-    print("Select rock(r), scissors(s), paper(p) or Q to quit.")
-    choice_user = input("Your choice: ").lower()
+word = "hammer"
+print(word)
+answer = ""
+string = ""
 
-    if choice_user not in ["r", "s", "p", "q"]:
-        print('Enter only r, s, p or q')
-        continue
-
-    print(f"Computer chose: {options[num]}")    
-
-    if choice_user == "r":
-        if options[num] == "Rock":
-            print("We tied.")
-        elif options[num] == "Scissors":
-            print("You won.")
-            score_user += 1
-        else:
-            print("I won.")
-            score_comp += 1
-
-    elif choice_user == "s":
-        if options[num] == "Scissors":
-            print("We tied.")
-        elif options[num] == "Paper":
-            print("You won.")
-            score_user += 1
-        else:
-            print("I won.")
-            score_comp += 1
-
-    elif choice_user == "p":
-        if options[num] == "paper":
-            print("We tied.")
-        elif options[num] == "Rock":
-            print("You won.")
-            score_user += 1
-        else:
-            print("I won.")
-            score_comp += 1
+while answer != word:
+    letter = input("Type a letter: ")
+    if letter in word:
+        ind_list = [idx for idx, item in enumerate(word) if letter in item]
+        print(ind_list)
     
-    else:
-        print(f"We'll end the game.  You scored: {score_user}. \
-            I scored: {score_comp}") 
-        break
+        for i in range(ind_list[0]):
+            string = string + "_ " 
+            
+        string = string + letter
 
-print("Bye!")
+        for i in range(len(word) - ind_list[-1] - 1):
+            string = string + "_ "
+        print(string)
+
+    
+
